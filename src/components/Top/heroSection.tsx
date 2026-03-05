798f2c661319073a0fd4e7ce054ee05a4f146c65
Chris <renzchris@web.de>
2026-03-05 17:19:03 +0100

    added subtitle

--- src/components/Top/heroSection.tsx
+++ src/components/Top/heroSection.tsx
@@ -7,31 +7,52 @@ import LogoAnimation from "./LogoAnimation";
 
 export default function HeroSection() {
   const pRef = useRef<HTMLParagraphElement>(null);
+  const textRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (!pRef.current) return;
+    const tl = gsap.timeline();
 
-    gsap.fromTo(
+    // h2 Texte einblenden
+    tl.fromTo(
+      textRef.current,
+      { opacity: 0, y: 30 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1.2,
+        delay: 4, // nach deinem Heading Effekt
+        ease: "power3.out",
+      }
+    );
+
+    // scroll text danach
+    tl.fromTo(
       pRef.current,
       { opacity: 0 },
       {
         opacity: 1,
-        duration: 1.5,
-        delay: 0.5,
+        duration: 1,
         ease: "power2.out",
       }
     );
   }, []);
 
   return (
-    <section className="relative h-screen bg-[#151515] text-white overflow-hidden ">
+    <section className="relative h-screen bg-[#151515] overflow-hidden">
       <div className="absolute inset-0 flex justify-center items-center">
         <LogoAnimation />
       </div>
+
       <div className="relative flex flex-col justify-center h-full px-8">
         <div className="text-left mx-auto">
           <HeadingEffect />
-          <div className="absolute bottom-0 left-0 w-full text-center pb-8">
+
+          <div ref={textRef} className="text-center text-[#828282] text-3xl pt-14 opacity-0">
+            <h2>Designing the experiences around them.</h2>
+            <h2>Developing modern websites & web apps.</h2>
+          </div>
+
+          <div className="absolute bottom-0 left-0 w-full text-center text-[#D9D9D9] pb-8">
             <p ref={pRef} style={{ opacity: 0 }}>
               scroll to explore <span>&darr;</span>
             </p>
